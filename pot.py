13907d4c703b9a2f74ee6480b7b06e73059f0085
Bonnie King <misterbonnie@gmail.com>
2013-04-09 16:42:07 -0500

    a nice module that can be imported

--- pot.py
+++ pot.py
@@ -12,39 +12,40 @@ GPIO.setmode(GPIO.BCM)
 DEBUG = 1
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+def readadc(adcnum=0, clockpin=18, mosipin=24, misopin=23, cspin=25):
+    
+    if ((adcnum > 7) or (adcnum < 0)):
+            return -1
+    GPIO.output(cspin, True)
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+            GPIO.output(mosipin, True)
+        else:
+            GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+        adcout <<= 1
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
 
-        GPIO.output(cspin, True)
+    GPIO.output(cspin, True)
         
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
+    adcout >>= 1       # first bit is 'null' so drop it
+    return adcout
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -94,3 +95,4 @@ if __name__ == "__main__":
 
         # save the potentiometer reading for the next loop
         last_read = trim_pot
+        time.sleep(0.5)
